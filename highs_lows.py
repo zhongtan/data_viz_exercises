607cd913ab62e000329aedb23b3a7980fb1f0abf
Zheng Hong Tan <zhongtan@uw.edu>
2019-03-26 23:52:30 -0700

    add code for plotting low temperatures

--- highs_lows.py
+++ highs_lows.py
@@ -1,25 +1,38 @@
 import csv
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 
 # Get high temperatures from file.
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
 
-  highs = []
+  dates, highs, lows = [], [], []
   for row in reader:
-    high = int(row[1])
-    highs.append(high)
+    try:
+      current_date = datetime.strptime(row[0], "%Y-%m-%d")
+      low = int(row[3])
+      high = int(row[1])
+    except ValueError:
+      print(current_date, "Missing data (low or high temperature)")
+    else:
+      dates.append(current_date)
+      highs.append(high)
+      lows.append(low)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=14)
+plt.title("Daily high temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()  # draws the labels diagonally to prevent overlapping
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
