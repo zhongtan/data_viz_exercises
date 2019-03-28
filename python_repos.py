1e83590fbb224ee6c413190cf50175ea88634131
Zheng Hong Tan <zhongtan@uw.edu>
2019-03-27 17:04:35 -0700

    minor additions

--- python_repos.py
+++ python_repos.py
@@ -14,10 +14,15 @@ print("Total repositories:", response_dict['total_count'])
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
   names.append(repo_dict['name'])
-  stars.append(repo_dict['stargazers_count'])
+
+  plot_dict = {
+    'value': repo_dict['stargazers_count'],
+    'label': repo_dict['description'],
+  }
+  plot_dicts.append(plot_dict)
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
@@ -25,5 +30,4 @@ chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
-chart.render_to_file('python_repos.svg')
+chart.add('', plot_dicts)
